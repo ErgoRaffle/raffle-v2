cdc195cdd397d4f10f8487949bed4304311eef41
Mohsen Javidpanah <moshen212@gmail.com>
2024-04-09 23:21:13 +0330

    New utilities and compile-all command implemented

--- packages/contracts/lib/index.ts
+++ packages/contracts/lib/index.ts
@@ -1,1 +1,97 @@
-import { Program } from "estree";
+import * as fs from "fs";
+import { exit } from "process";
+
+import { program } from "commander";
+
+import { logger } from "./logger.js";
+import { compileAll, ContextVarsType } from "./utils.js";
+
+
+program.name("contracts")
+    .description("CLI to do operations related to contracts");
+
+// compile-all related options
+program.command("compile-contracts")
+.option("-o, --output <output file path>", "Address of output file that contains contracts addresses")
+.option("-i, --input <intput file path>", "Address of intput file that contains JSON contract name and variables")
+.action((options) => {
+    // const options = program.opts()
+
+    logger.info("compile-all command started");
+
+    let inputs = {};
+    if(options.input) {
+        let inputContent = ""
+        try {
+            inputContent = fs.readFileSync(options.input).toString();
+        } catch(err) {
+            logger.error(`Error: ${err}`);
+            logger.info("compile-all failed");
+            throw err;
+        }
+
+        try {
+            inputs = JSON.parse(inputContent);
+        } catch(err) {
+            logger.error(`Error: Input file is not json`);
+            logger.info("compile-all failed");
+            exit(1);
+        }
+    }
+
+    let contracts = {}
+    try {
+        contracts = compileAll(inputs as ContextVarsType);
+    } catch(err) {
+        logger.error(`Error: ${err}`);
+        logger.info("compile-all failed");
+        exit(1);
+    }
+
+    if(options.output) {
+        try {
+            fs.writeFileSync(
+                options.output,
+                JSON.stringify(contracts, null, 4)
+            );
+        } catch(err) {
+            logger.error(`The compileAll function raised error: ${err}`);
+            throw err;
+        }
+    } else {
+        console.log(JSON.stringify(contracts, null, 4));
+    }
+
+    logger.info("compile-all ran successfull");
+});
+
+// Create template file of input variables
+program.command("make-input-template")
+.argument("<destination>", "Destination address of file")
+.action((destination) => {
+    logger.info("Create input file template started");
+    let scriptVariables = {
+        "service": {},
+        "inactiveRaffle": {},
+        "ticketRepo": {},
+        "activeRaffle": {},
+        "winner": {},
+        "ticket": {},
+        "successRaffle": {},
+        "winnerPrize": {},
+        "gift": {},
+        "giftRedeem": {},
+        "ticketRedeem": {}
+    };
+
+    try {
+        fs.writeFileSync(destination, JSON.stringify(scriptVariables, null, 4));
+    } catch(err) {
+        logger.error(`Error: ${err}`);
+        logger.info('Create input file template failed');
+    }
+
+    logger.info('Create input file template ran successfull');
+});
+
+program.parse();
--- packages/contracts/lib/utils.ts
+++ packages/contracts/lib/utils.ts
@@ -1,4 +1,10 @@
-const scripts = [
+import * as fs from 'fs';
+import { compile } from '@fleet-sdk/compiler';
+
+import { logger } from './logger.js';
+
+
+const scriptList = [
     'service',
     'inactiveRaffle',
     'ticketRepo',
@@ -9,21 +15,35 @@ const scripts = [
     'winnerPrize',
     'gift',
     'giftRedeem',
-    'ticketRedeem',
-    'ownerNft',
-    'serviceNft',
-    'raffleLicense',
-    'ticketToken',
-    'giftToken'
+    'ticketRedeem'
 ]
 
+export type ScriptNamesType = 'service' | 'inactiveRaffle' | 'ticketRepo' | 'activeRaffle' |
+                    'winner' | 'ticket' | 'successRaffle' | 'winnerPrize' |
+                    'gift' | 'giftRedeem' | 'ticketRedeem';
+export type ContextVarsType = Map<ScriptNamesType, Map<string, string>>;
 
-class ErgoScriptVars {
-    constructor() {
 
+export function compileAll(contextVars?: ContextVarsType): Map<ScriptNamesType, string> {
+    let contracts: Map<ScriptNamesType, string> = new Map<ScriptNamesType, string>();
+
+    for(const scriptName of scriptList) {
+        const scriptVars = contextVars !== undefined
+            ? (contextVars.get(scriptName as ScriptNamesType) || new Map<string, string>())
+            : new Map<string, string>();
+        let script: string = fs.readFileSync(`./scripts/${scriptName}.es`,'utf8');
+        for(const v of scriptVars) {
+            script = script.replace(v[0], v[1]);
+        }
+        try {
+            let contract = compile(script)
+            contracts.set(scriptName as ScriptNamesType, contract.toAddress().toString());
+        } catch(err) {
+            logger.error(`The compileAll function raised error: ${err}`);
+            throw err;
+        }
     }
-}
+    logger.info(`The compileAll function done successful`);
 
-export function compileAll(vars: Map<string, Map<string, string>>, asTestnet: boolean = false) {
-
+    return contracts;
 }
