62edfcdfccb6b7aceca2fca2589446ce0c6b17d8
Mohsen Javidpanah <moshen212@gmail.com>
2024-04-11 19:09:05 +0330

    Many improvement applied to utilities

--- packages/contracts/lib/utils.ts
+++ packages/contracts/lib/utils.ts
@@ -1,8 +1,11 @@
 import * as fs from 'fs';
+import * as path from 'node:path';
 import { compile } from '@fleet-sdk/compiler';
 
 import { logger } from './logger.js';
 
+
+const __dirname = path.resolve(path.dirname(''));
 
 const scriptList = [
     'service',
@@ -24,20 +27,22 @@ export type ScriptNamesType = 'service' | 'inactiveRaffle' | 'ticketRepo' | 'act
 export type ContextVarsType = Map<ScriptNamesType, Map<string, string>>;
 
 
-export function compileAll(contextVars?: ContextVarsType): Map<ScriptNamesType, string> {
-    let contracts: Map<ScriptNamesType, string> = new Map<ScriptNamesType, string>();
+export function compileAll(contextVars?: ContextVarsType): Object {
+    let contracts: {[key: string]: string} = {};
 
     for(const scriptName of scriptList) {
         const scriptVars = contextVars !== undefined
-            ? (contextVars.get(scriptName as ScriptNamesType) || new Map<string, string>())
-            : new Map<string, string>();
-        let script: string = fs.readFileSync(`./scripts/${scriptName}.es`,'utf8');
-        for(const v of scriptVars) {
-            script = script.replace(v[0], v[1]);
-        }
+        ? (contextVars.get(scriptName as ScriptNamesType) || new Map<string, string>())
+        : new Map<string, string>();
+        let script: string = fs.readFileSync(
+            path.join(__dirname, `lib/scripts/${scriptName}.es`), 'utf8');
+
+        for(const nameAndValue of Object.entries(scriptVars))
+            script = script.replace(nameAndValue[0], nameAndValue[1]);
+
         try {
-            let contract = compile(script)
-            contracts.set(scriptName as ScriptNamesType, contract.toAddress().toString());
+            let contract = compile(script, {});
+            contracts[scriptName] = contract.toAddress().toString();
         } catch(err) {
             logger.error(`The compileAll function raised error: ${err}`);
             throw err;
