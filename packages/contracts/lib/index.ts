151c1c378b594bbc4699b0e5bd5bfa4cb61f4727
Mohsen Javidpanah <moshen212@gmail.com>
2024-04-22 16:02:03 +0330

    Utilities related to compile contracts added

--- packages/contracts/lib/index.ts
+++ packages/contracts/lib/index.ts
@@ -1,95 +1,102 @@
-import * as fs from "fs";
-import { exit } from "process";
+import * as fs from 'fs';
+import { exit } from 'process';
 
-import { program } from "commander";
+import { program } from 'commander';
 
-import { logger } from "./logger.js";
-import { compileAll, ContextVarsType } from "./utils.js";
+import { logger } from './logger';
+import { compileAll, ContextVarsType } from './utils';
 
-
-program.name("contracts")
-    .description("CLI to do operations related to contracts");
+program
+  .name('contracts')
+  .description('CLI to do operations related to contracts');
 
 // compile-all related options
-program.command("compile-contracts")
-.option("-o, --output <output file path>", "Address of output file that contains contracts addresses")
-.option("-i, --input <intput file path>", "Address of intput file that contains JSON contract name and variables")
-.action((options) => {
-    logger.info("compile-all command started");
+program
+  .command('compile-contracts')
+  .option(
+    '-o, --output <output file path>',
+    'Address of output file that contains contracts addresses',
+  )
+  .option(
+    '-i, --input <intput file path>',
+    'Address of intput file that contains JSON contract name and variables',
+  )
+  .action((options) => {
+    logger.info('compile-all command started');
 
     let inputs: ContextVarsType = new Map();
-    if(options.input) {
-        let inputContent = ""
-        try {
-            inputContent = fs.readFileSync(options.input).toString();
-        } catch(err) {
-            logger.error(`Error: ${err}`);
-            logger.info("compile-all command failed");
-            exit(1);
-        }
-
-        try {
-            inputs = new Map(Object.entries(JSON.parse(inputContent))) as ContextVarsType;
-        } catch(err) {
-            logger.error(`Error: Input file is not json`);
-            logger.info("compile-all command failed");
-            exit(1);
-        }
-    }
-
-    let contracts = {}
-    try {
-        contracts = compileAll(inputs);
-    } catch(err) {
+    if (options.input) {
+      let inputContent = '';
+      try {
+        inputContent = fs.readFileSync(options.input).toString();
+      } catch (err) {
         logger.error(`Error: ${err}`);
-        logger.info("compile-all failed");
+        logger.info('compile-all command failed');
         exit(1);
+      }
+
+      try {
+        inputs = new Map(
+          Object.entries(JSON.parse(inputContent)),
+        ) as ContextVarsType;
+      } catch (err) {
+        logger.error(`Error: Input file is not json`);
+        logger.info('compile-all command failed');
+        exit(1);
+      }
     }
 
-    if(options.output) {
-        try {
-            fs.writeFileSync(
-                options.output,
-                JSON.stringify(contracts, null, 4)
-            );
-        } catch(err) {
-            logger.error(`The compileAll function raised error: ${err}`);
-            throw err;
-        }
+    let contracts = {};
+    try {
+      contracts = compileAll(inputs);
+    } catch (err) {
+      logger.error(`Error: ${err}`);
+      logger.info('compile-all failed');
+      exit(1);
+    }
+
+    if (options.output) {
+      try {
+        fs.writeFileSync(options.output, JSON.stringify(contracts, null, 4));
+      } catch (err) {
+        logger.error(`The compileAll function raised error: ${err}`);
+        throw err;
+      }
     } else {
-        console.log(JSON.stringify(contracts, null, 4));
+      console.log(JSON.stringify(contracts, null, 4));
     }
 
-    logger.info("compile-all command ran successfull");
-});
+    logger.info('compile-all command ran successfull');
+  });
 
 // Create template file of input variables
-program.command("make-input-template")
-.argument("<destination>", "Destination address of file")
-.action((destination) => {
-    logger.info("Create input file template started");
-    let scriptVariables = {
-        "service": {},
-        "inactiveRaffle": {},
-        "ticketRepo": {},
-        "activeRaffle": {},
-        "winner": {},
-        "ticket": {},
-        "successRaffle": {},
-        "winnerPrize": {},
-        "gift": {},
-        "giftRedeem": {},
-        "ticketRedeem": {}
+program
+  .command('make-input-template')
+  .argument('<destination>', 'Destination address of file')
+  .action((destination) => {
+    logger.info('Create input file template started');
+    const scriptVariables = {
+      service: {},
+      inactiveRaffle: {},
+      ticketRepo: {},
+      activeRaffle: {},
+      winner: {},
+      ticket: {},
+      successRaffle: {},
+      winnerPrize: {},
+      gift: {},
+      giftRedeem: {},
+      ticketRedeem: {},
     };
 
     try {
-        fs.writeFileSync(destination, JSON.stringify(scriptVariables, null, 4));
-    } catch(err) {
-        logger.error(`Error: ${err}`);
-        logger.info('Create input file template failed');
+      fs.writeFileSync(destination, JSON.stringify(scriptVariables, null, 4));
+    } catch (err) {
+      logger.error(`Error: ${err}`);
+      logger.info('Create input file template failed');
     }
 
     logger.info('Create input file template command ran successfull');
-});
+  });
 
 program.parse();
--- packages/contracts/lib/utils.ts
+++ packages/contracts/lib/utils.ts
@@ -2,53 +2,66 @@ import * as fs from 'fs';
 import * as path from 'node:path';
 import { compile } from '@fleet-sdk/compiler';
 
-import { logger } from './logger.js';
-
+import { logger } from './logger';
 
 const __dirname = path.resolve(path.dirname(''));
 
 const scriptList = [
-    'service',
-    'inactiveRaffle',
-    'ticketRepo',
-    'activeRaffle',
-    'winner',
-    'ticket',
-    'successRaffle',
-    'winnerPrize',
-    'gift',
-    'giftRedeem',
-    'ticketRedeem'
-]
+  'service',
+  'inactiveRaffle',
+  'ticketRepo',
+  'activeRaffle',
+  'winner',
+  'ticket',
+  'successRaffle',
+  'winnerPrize',
+  'gift',
+  'giftRedeem',
+  'ticketRedeem',
+];
 
-export type ScriptNamesType = 'service' | 'inactiveRaffle' | 'ticketRepo' | 'activeRaffle' |
-                    'winner' | 'ticket' | 'successRaffle' | 'winnerPrize' |
-                    'gift' | 'giftRedeem' | 'ticketRedeem';
+export type ScriptNamesType =
+  | 'service'
+  | 'inactiveRaffle'
+  | 'ticketRepo'
+  | 'activeRaffle'
+  | 'winner'
+  | 'ticket'
+  | 'successRaffle'
+  | 'winnerPrize'
+  | 'gift'
+  | 'giftRedeem'
+  | 'ticketRedeem';
 export type ContextVarsType = Map<ScriptNamesType, Map<string, string>>;
 
+export function compileAll(contextVars?: ContextVarsType): {
+  [key: string]: string;
+} {
+  const contracts: { [key: string]: string } = {};
 
-export function compileAll(contextVars?: ContextVarsType): Object {
-    let contracts: {[key: string]: string} = {};
-
-    for(const scriptName of scriptList) {
-        const scriptVars = contextVars !== undefined
-        ? (contextVars.get(scriptName as ScriptNamesType) || new Map<string, string>())
+  for (const scriptName of scriptList) {
+    const scriptVars =
+      contextVars !== undefined
+        ? contextVars.get(scriptName as ScriptNamesType) ||
+          new Map<string, string>()
         : new Map<string, string>();
-        let script: string = fs.readFileSync(
-            path.join(__dirname, `lib/scripts/${scriptName}.es`), 'utf8');
+    let script: string = fs.readFileSync(
+      path.join(__dirname, `lib/scripts/${scriptName}.es`),
+      'utf8',
+    );
 
-        for(const nameAndValue of Object.entries(scriptVars))
-            script = script.replace(nameAndValue[0], nameAndValue[1]);
+    for (const nameAndValue of Object.entries(scriptVars))
+      script = script.replace(nameAndValue[0], nameAndValue[1]);
 
-        try {
-            let contract = compile(script, {});
-            contracts[scriptName] = contract.toAddress().toString();
-        } catch(err) {
-            logger.error(`The compileAll function raised error: ${err}`);
-            throw err;
-        }
+    try {
+      const contract = compile(script, {});
+      contracts[scriptName] = contract.toAddress().toString();
+    } catch (err) {
+      logger.error(`The compileAll function raised error: ${err}`);
+      throw err;
     }
-    logger.info(`The compileAll function done successful`);
+  }
+  logger.info(`The compileAll function done successful`);
 
-    return contracts;
+  return contracts;
 }
