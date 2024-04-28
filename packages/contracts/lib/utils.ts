7d3cb7e282631ccb306c9decd0720684ec3ac882
Mohsen Javidpanah <moshen212@gmail.com>
2024-04-28 12:56:40 +0330

    Some docstring added

--- packages/contracts/lib/utils.ts
+++ packages/contracts/lib/utils.ts
@@ -22,6 +22,16 @@ const scriptList = [
   'ticketRedeem',
 ];
 
+/**
+ * Returns all of compiled Raffle-v2 contracts
+ *
+ * @remarks
+ * This method is part of the {@link raffle-v2#contracts | contracts subsystem}.
+ *
+ * @param contextVars - variables of raffle-v2 scripts
+ * @param logger - logger object
+ * @returns object that contains compiled contracts
+ */
 export function compileAll(
   contextVars?: ContextVarsType,
   logger: Logger = defaultLogger,
