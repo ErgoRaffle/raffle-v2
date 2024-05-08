00593e87adc9642e924add2557d25ae85e23f6ab
Mohsen Javidpanah <moshen212@gmail.com>
2024-05-08 12:04:38 +0330

    Some script related constants updated

--- packages/contracts/constants.ts
+++ packages/contracts/constants.ts
@@ -44,8 +44,6 @@ export const scriptsRequireAddresses: {
 export const defaultScriptsVariables = {
   service: {
     OWNER_NFT_B64: '',
-    // INACTIVE_RAFFLE_SCRIPT_HASH_B64: '',
-    // TICKET_REPO_SCRIPT_HASH_B64: '',
     FEE: constants.DEFAULT_FEE,
     MIN_BOX_VALUE: SAFE_MIN_BOX_VALUE,
   },
