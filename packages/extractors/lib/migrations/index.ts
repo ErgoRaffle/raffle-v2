088cfa26500d0b246de8c961fa464780e5ccdb81
Mohsen Javidpanah <moshen212@gmail.com>
2025-02-15 13:50:11 +0330

    Some changes Applied

--- packages/extractors/lib/migrations/index.ts
+++ packages/extractors/lib/migrations/index.ts
@@ -1,7 +1,7 @@
-// import { Sqlite1736703916775 } from '../migrations/sqlite/1736703916775-sqlite';
+import { Sqlite1736703916775 } from '../migrations/sqlite/1736703916775-sqlite';
 import { Postgres1739023898594 } from '../migrations/postgres/1739023898594-postgres';
 
 export const migrations = {
-  sqlite: [],
+  sqlite: [Sqlite1736703916775],
   postgres: [Postgres1739023898594],
 };
--- packages/extractors/lib/migrations/postgres/1739023898594-postgres.ts
+++ packages/extractors/lib/migrations/postgres/1739023898594-postgres.ts
@@ -23,25 +23,6 @@ export class Postgres1739023898594 implements MigrationInterface {
             )
         `);
     await queryRunner.query(`
-            CREATE TABLE "block_entity" (
-                "id" SERIAL NOT NULL,
-                "height" integer NOT NULL,
-                "hash" character varying NOT NULL,
-                "parentHash" character varying NOT NULL,
-                "status" character varying NOT NULL,
-                "extra" character varying,
-                "scanner" character varying NOT NULL,
-                "timestamp" integer NOT NULL,
-                "year" integer,
-                "month" integer,
-                "day" integer,
-                CONSTRAINT "UQ_7e20625b11840edf7f120565c3d" UNIQUE ("parentHash", "scanner"),
-                CONSTRAINT "UQ_b1e24c5950a7c3dd48d92bbfbb2" UNIQUE ("hash", "scanner"),
-                CONSTRAINT "UQ_521d830047d5fe08988538289dd" UNIQUE ("height", "scanner"),
-                CONSTRAINT "PK_c3ddd57793960562837e8a402f1" PRIMARY KEY ("id")
-            )
-        `);
-    await queryRunner.query(`
             CREATE TABLE "extractor_status_entity" (
                 "scannerId" character varying NOT NULL,
                 "extractorId" character varying NOT NULL,
@@ -55,9 +36,6 @@ export class Postgres1739023898594 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             DROP TABLE "extractor_status_entity"
-        `);
-    await queryRunner.query(`
-            DROP TABLE "block_entity"
         `);
     await queryRunner.query(`
             DROP TABLE "raffle_service"
