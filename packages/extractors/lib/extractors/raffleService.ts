a96594b09f2c1d041a9a0362c0fd89c8fec10f50
Mohsen Javidpanah <moshen212@gmail.com>
2025-02-22 09:07:11 +0330

    Tiny change applied

--- packages/extractors/lib/extractors/raffleService.ts
+++ packages/extractors/lib/extractors/raffleService.ts
@@ -20,7 +20,7 @@ export class RaffleServiceExtractor extends AbstractInitializableErgoExtractor<
   readonly actions: RaffleServiceAction;
   private readonly id: string;
   private readonly networkType: Network;
-  private readonly ergoTree?: string;
+  private readonly ergoTree: string;
   private readonly serviceNFTId: string;
 
   constructor(
@@ -37,9 +37,7 @@ export class RaffleServiceExtractor extends AbstractInitializableErgoExtractor<
     super(type, url, address, logger, initialize);
     this.id = id;
     this.networkType = networkType;
-    this.ergoTree = address
-      ? ErgoAddress.fromBase58(address).ergoTree.toString()
-      : undefined;
+    this.ergoTree = ErgoAddress.fromBase58(address).ergoTree.toString();
     this.serviceNFTId = serviceNFTId;
     this.actions = new RaffleServiceAction(dataSource, this.logger);
   }
