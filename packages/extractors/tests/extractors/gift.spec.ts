0c4cfac5bd6cef926e4f1884cb25b7e12ee81a85
Mohsen Javidpanah <moshen212@gmail.com>
2025-03-12 16:35:45 +0330

    Tiny change on the GiftExtractor tests applied

--- packages/extractors/tests/extractors/gift.spec.ts
+++ packages/extractors/tests/extractors/gift.spec.ts
@@ -78,7 +78,7 @@ describe('GiftExtractor', () => {
      * - call the extractBoxData functions
      * - check if Gift box data extracted correctly
      * @expected
-     * - Gifts should fail
+     * - Gifts should not extract successfully
      */
     extractorTest(
       `should fail extracting data from a sample gift box and by empty extension value`,
