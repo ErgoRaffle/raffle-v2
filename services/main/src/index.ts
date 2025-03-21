a97a12c16c0059d5772a18bc70d75ece14f90428
Mohsen Javidpanah <moshen212@gmail.com>
2025-03-21 13:18:37 +0330

    Some code related to database creation & migration removed

--- services/main/src/index.ts
+++ services/main/src/index.ts
@@ -2,11 +2,7 @@ import './bootstrap';
 
 import startApp from './app';
 
-import { dataSource } from './dataSources/sqlite';
-
 const main = async () => {
-  await dataSource.initialize();
-  await dataSource.runMigrations();
   startApp();
 };
 
