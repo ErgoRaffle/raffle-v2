1519fe2a54f746b9f6b59890be8ea47b27cfa93d
fateme.r <fatemeh.r3366@gmail.com>
2025-04-19 15:31:09 +0330

    Add gift builder

---
+++ packages/boxes/lib/builders/giftBuilder.ts
@@ -0,0 +1,212 @@
+import {
+  OutputBuilder,
+  TokenAmount,
+  SColl,
+  SByte,
+  SInt,
+  SLong,
+  ErgoAddress,
+  Box,
+  Amount,
+} from '@fleet-sdk/core';
+import { SConstant } from '@fleet-sdk/serializer';
+import { raffleInfo } from '@ergo-raffle/contracts';
+import { blake2b256 } from '@fleet-sdk/crypto';
+
+/**
+ * Builder class for creating Gift boxes in the ErgoRaffle protocol
+ * Gift box represents a gift token that can be unwrapped or returned
+ *
+ * Registers:
+ *   R4[Coll[Byte]]: [DonatorErgoTreeHash]
+ *   R5[Int]: WinnerIndex
+ *   R6[Long]: txFee
+ * Tokens:
+ *   0: GiftToken
+ *   1..n: Gifts (array of tokens)
+ */
+export class GiftBuilder {
+  // Private fields
+  private value?: bigint;
+  private creationHeight?: number;
+  private donatorErgoTreeHash?: Uint8Array;
+  private winnerIndex?: number;
+  private txFee?: bigint;
+  private giftToken?: TokenAmount<bigint>;
+  private gifts: TokenAmount<bigint>[] = [];
+
+  /**
+   * Set the box value in nanoERG
+   * @param value - Amount in nanoERG
+   * @returns this builder instance
+   */
+  setValue = (value: bigint): this => {
+    this.value = value;
+    return this;
+  };
+
+  /**
+   * Set the creation height for the box
+   * @param height - Block height
+   * @returns this builder instance
+   */
+  setCreationHeight = (height: number): this => {
+    this.creationHeight = height;
+    return this;
+  };
+
+  /**
+   * Set the donator's address and process it to get ErgoTree hash
+   * @param address - Base58 encoded Ergo address
+   * @returns this builder instance
+   */
+  setDonatorAddress = (address: string): this => {
+    const donatorErgoAddress = ErgoAddress.fromBase58(address);
+    this.donatorErgoTreeHash = blake2b256(
+      Buffer.from(donatorErgoAddress.ergoTree, 'hex'),
+    );
+    return this;
+  };
+
+  /**
+   * Set the winner index
+   * @param index - Winner index
+   * @returns this builder instance
+   */
+  setWinnerIndex = (index: number): this => {
+    this.winnerIndex = index;
+    return this;
+  };
+
+  /**
+   * Set the transaction fee
+   * @param fee - Fee amount in nanoERG
+   * @returns this builder instance
+   */
+  setTxFee = (fee: bigint): this => {
+    this.txFee = fee;
+    return this;
+  };
+
+  /**
+   * Set the gift token identifier
+   * @param tokenId - Gift token ID
+   * @returns this builder instance
+   */
+  setGiftToken = (tokenId: string): this => {
+    this.giftToken = {
+      tokenId,
+      amount: 1n,
+    };
+    return this;
+  };
+
+  /**
+   * Add a gift token to the array
+   * @param tokenId - Gift token ID
+   * @param amount - Number of gifts
+   * @returns this builder instance
+   */
+  addGift = (tokenId: string, amount: bigint): this => {
+    this.gifts.push({
+      tokenId,
+      amount,
+    });
+    return this;
+  };
+
+  /**
+   * Add multiple gift tokens as a batch
+   * @param gifts - Array of gift tokens to add
+   * @returns this builder instance
+   */
+  addGifts = (gifts: TokenAmount<bigint>[]): this => {
+    this.gifts.push(...gifts);
+    return this;
+  };
+
+  /**
+   * Clear all gift tokens
+   * @returns this builder instance
+   */
+  clearGifts = (): this => {
+    this.gifts = [];
+    return this;
+  };
+
+  /**
+   * Set the donator's ErgoTree hash
+   * @param hash - Donator's ErgoTree hash as byte array
+   * @returns this builder instance
+   */
+  setDonatorErgoTreeHash = (hash: Uint8Array): this => {
+    this.donatorErgoTreeHash = hash;
+    return this;
+  };
+
+  /**
+   * Validate that all required parameters are set
+   * @throws Error if any required parameter is missing
+   */
+  private validate = (): void => {
+    if (!this.value) throw new Error('Value not set');
+    if (!this.creationHeight) throw new Error('Creation height not set');
+    if (!this.donatorErgoTreeHash)
+      throw new Error('Donator ErgoTree hash not set');
+    if (!this.winnerIndex) throw new Error('Winner index not set');
+    if (!this.txFee) throw new Error('Transaction fee not set');
+    if (!this.giftToken) throw new Error('Gift token not set');
+    if (this.gifts.length === 0) throw new Error('No gifts added');
+  };
+
+  /**
+   * Build an output box for the gift contract
+   * Contains gift token and array of gifts
+   * @returns OutputBuilder instance configured for the gift box
+   * @throws Error if any required parameter is missing
+   */
+  buildOutput = (): OutputBuilder => {
+    this.validate();
+
+    return new OutputBuilder(
+      this.value!,
+      ErgoAddress.fromBase58(raffleInfo.addresses.gift).ergoTree,
+      this.creationHeight!,
+    )
+      .addTokens([this.giftToken!, ...this.gifts])
+      .setAdditionalRegisters({
+        R4: SColl(SByte, Array.from(this.donatorErgoTreeHash!)).toHex(),
+        R5: SInt(this.winnerIndex!).toHex(),
+        R6: SLong(this.txFee!).toHex(),
+      });
+  };
+
+  /**
+   * Fill private parameters from a winner box
+   * @param winnerBox - Winner box to extract information from
+   * @returns this builder instance
+   * @throws Error if box structure doesn't match winner box requirements
+   */
+  giftForWinner = (winnerBox: Box<Amount>): this => {
+    if (winnerBox.assets.length < 2) {
+      throw new Error('Invalid winner box: missing required tokens');
+    }
+
+    const registers = winnerBox.additionalRegisters;
+    if (!registers.R4 || !registers.R5 || !registers.R6) {
+      throw new Error('Invalid winner box: missing required registers');
+    }
+
+    // Extract register data using SConstant
+    const r4Data = SConstant.from(registers.R4).data as bigint[];
+    const winnerIndex = SConstant.from(registers.R5).data as number;
+
+    // Set all parameters using setters
+    this.setValue(2n * r4Data[2]) // 2 * txFee from R4[2]
+      .setWinnerIndex(winnerIndex)
+      .setTxFee(r4Data[2]) // txFee from R4[2]
+      .setGiftToken(winnerBox.assets[1].tokenId);
+
+    return this;
+  };
+}
