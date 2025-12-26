95e71094d89cbed88eee7008377b721dfd830350
Mehran Aghabozorgi <mehranagh200@gmail.com>
2025-12-25 18:50:45 -0800

    Fix boxLookupService name

--- services/background-job/src/services/boxLookup/index.ts
+++ services/background-job/src/services/boxLookup/index.ts
@@ -1,2 +1,2 @@
 export { deserializeTxForBoxLookup, toBoxLookupRequest } from './compat';
-export { BoxLookupService } from './boxLoookupService';
+export { BoxLookupService } from './boxLookupService';
--- services/background-job/src/services/initializerService.ts
+++ services/background-job/src/services/initializerService.ts
@@ -13,7 +13,7 @@ import dataSource from '../dataSource';
 import { ScannerService } from './scannerService';
 import { TxPotService } from './txPotService';
 import { HealthCheckService } from './healthCheckService';
-import { BoxLookupService } from './boxLookup/boxLoookupService';
+import { BoxLookupService } from './boxLookup/boxLookupService';
 import { ApiService } from './apiService';
 import { CreationService } from './transactions/creationService';
 import { ActivationService } from './transactions/activationService';
