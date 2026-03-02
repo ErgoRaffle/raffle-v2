fff9cb8eaf0ca8ccfd1b6d7e0eac2761ce236840
fateme.r <fatemeh.r3366@gmail.com>
2026-03-03 01:17:02 +0330

    initialize btc-payment package

---
+++ services/btc-payment/README.md
@@ -0,0 +1,14 @@
+# btc-payment
+
+## Table of contents
+
+- [Introduction](#introduction)
+- [Usage](#usage)
+
+## Introduction
+
+Manages direct btc payments for ErgoRaffle
+
+## Usage
+
+TBD
---
+++ services/btc-payment/src/bootstrap.ts
@@ -0,0 +1,16 @@
+import { CallbackLoggerFactory } from '@rosen-bridge/callback-logger';
+import WinstonLogger from '@rosen-bridge/winston-logger';
+
+import { maxLogSize, maxLogFilesCount, logsPath, logLevel } from './configs';
+
+CallbackLoggerFactory.init(
+  new WinstonLogger([
+    {
+      type: 'file',
+      path: logsPath,
+      maxSize: maxLogSize,
+      maxFiles: maxLogFilesCount,
+      level: logLevel,
+    },
+  ]),
+);
