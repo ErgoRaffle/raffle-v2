1b30e20226df9e2bdb987b8aa29eaeae914adb7d
fateme.r <fatemeh.r3366@gmail.com>
2026-04-01 17:21:26 +0330

    move fleet box selection to the new package

--- packages/proxy-transactions/lib/index.ts
+++ packages/proxy-transactions/lib/index.ts
@@ -1,4 +1,3 @@
 export { CreationProxyTxBuilder } from './creationProxyTxBuilder';
 export { DonationProxyTxBuilder } from './donationProxyTxBuilder';
 export { AddGiftProxyTxBuilder } from './addGiftProxyTxBuilder';
-export { FleetBoxSelection } from './fleetBoxSelection';
